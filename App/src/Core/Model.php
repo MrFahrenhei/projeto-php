ab0ee755efa5a93777eaca8581c719e3ba31bcac
Vinícius Valle Beraldo <vvberaldo@proton.me>
2025-05-05 17:07:09 -0300

    criando métodos para achar vários no banco, um só, atualizar ou deletar um

--- App/src/Core/Model.php
+++ App/src/Core/Model.php
@@ -2,7 +2,11 @@
 
 namespace App\Core;
 
+use App\Exceptions\HomeNotFoundException;
+use App\Models\Home;
+use PDO;
 use PDOStatement;
+use stdClass;
 
 abstract class Model extends Rules
 {
@@ -28,8 +32,91 @@ abstract class Model extends Rules
        $this->{$this->primaryKey()} = App::$app->db->pdo->lastInsertId();
        return $result;
     }
+
+    public function findMany(mixed $where): array
+    {
+       $tableName = $this->tableName();
+       $attributes = array_keys($where);
+       $sql = $this->implodeMany($attributes);
+       $stmt = self::prepare("SELECT * FROM $tableName WHERE $sql");
+       foreach($where as $attribute => $value) {
+          $stmt->bindValue(":$attribute", $value);
+       }
+       $stmt->execute();
+       return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    /**
+     * @throws HomeNotFoundException
+     */
+    public function findOne(mixed $where, $tableName = null, $className = null): Home
+    {
+        if(is_null($tableName)) {
+            $tableName = static::tableName();
+        }
+        if(is_null($className)) {
+            $className = static::class;
+        }
+        $attributes = array_keys($where);
+        $sql = $this->implodeMany($attributes);
+        $stmt = self::prepare("SELECT * FROM $tableName WHERE $sql");
+        foreach($where as $attribute => $value) {
+            $stmt->bindValue(":$attribute", $value);
+        }
+        $stmt->execute();
+        $obj = $stmt->fetchObject($className);
+        return ($obj) ?: throw new HomeNotFoundException();
+    }
+
+    public function deleteOne(mixed $seted, mixed $where, $tableName = null): bool
+    {
+        if(is_null($tableName)) {
+            $tableName = static::tableName();
+        }
+        $setIT = array_keys($seted);
+        $whereIt = array_keys($where);
+        $setedSql = $this->implodeMany($setIT);
+        $whereSql = $this->implodeMany($whereIt);
+        $stmt = self::prepare("UPDATE $tableName SET $setedSql WHERE $whereSql");
+        $this->bindValues($stmt, $where);
+        $this->bindValues($stmt, $seted);
+        return $stmt->execute();
+    }
+
+    public function updateOne(array $seted, mixed $where, $tableName = null): bool
+    {
+        if(is_null($tableName)) {
+            $tableName = static::tableName();
+        }
+        $whereIt = array_keys($where);
+        $setIt = array_keys($seted);
+        $setedSQL = $this->implodeMany($setIt, ' , ');
+        $whereSQL = $this->implodeMany($whereIt);
+        $stmt = self::prepare("UPDATE $tableName SET $setedSQL WHERE $whereSQL");
+        $this->bindValues($stmt, $where);
+        $this->bindValues($stmt, $seted);
+        return $stmt->execute();
+    }
     public function prepare(string $sql): PDOStatement
     {
         return App::$app->db->pdo->prepare($sql);
+    }
+    private function implodeMany(mixed $values, string $div = " AND "): string
+    {
+        return implode($div, array_map(fn($attr) => "$attr = :$attr", $values));
+    }
+    private function bindValues(PDOStatement $stmt, array $data): void
+    {
+        foreach ($data as $key => $value) {
+            $paramType = PDO::PARAM_STR;
+            if (is_bool($value)) {
+                $paramType = PDO::PARAM_BOOL;
+            } elseif (is_int($value)) {
+                $paramType = PDO::PARAM_INT;
+            } elseif (is_null($value)) {
+                $paramType = PDO::PARAM_NULL;
+            }
+            $stmt->bindValue(":$key", $value, $paramType);
+        }
     }
 }
