ee1468468eab6c0475a6da180b58f73a7d136840
Vinícius Valle Beraldo <vvberaldo@proton.me>
2025-05-05 16:59:26 -0300

    fix: changing file name

--- App/migrations.php
+++ App/migrations.php
@@ -7,4 +7,4 @@ require_once __DIR__.'/vendor/autoload.php';
 $app = new App(__DIR__);
 $app->db->applyMigrations();
 
-//docker exec -it php_service php /var/www/html/Migrations.php
+//docker exec -it php_service php /var/www/html/migrations.php
