73fcbefa03e24bcb5fa7ee355e57162a692501c9
Vinícius Valle Beraldo <vvberaldo@proton.me>
2025-05-05 17:00:05 -0300

    criando as rotas para o CRUD do home Controller ou cadastro de residência

--- App/public/index.php
+++ App/public/index.php
@@ -1,15 +1,21 @@
 <?php
 
 use App\Controllers\AuthController;
-use App\Controllers\Home;
+use App\Controllers\HomeController;
+use App\Controllers\Inicio;
 use App\Core\App;
 
 require_once dirname(__DIR__, 1).'/vendor/autoload.php';
 
 $app = new App(dirname(__DIR__, 1));
-$app->router->get("/home", [Home::class, 'functionGet']);
+$app->router->get("/home", [Inicio::class, 'functionGet']);
 $app->router->get("/app", function(){return "this is get app";});
 $app->router->post("/app", function(){return "this is post app";});
-$app->router->post("/home", [Home::class, 'functionPost']);
+$app->router->post("/home", [Inicio::class, 'functionPost']);
 $app->router->post("/sign", [AuthController::class, 'createCustomer']);
+$app->router->post("/insertHome", [HomeController::class, 'insertHome']);
+$app->router->get("/getAllHome", [HomeController::class, 'getAllHome']);
+$app->router->get("/getSingleHome", [HomeController::class, 'getSingleHome']);
+$app->router->post("/updateHome", [HomeController::class, 'updateHome']);
+$app->router->post("/deleteHome", [HomeController::class, 'deleteHome']);
 echo $app->run();
