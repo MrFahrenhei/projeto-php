db1635a87bba4eb50435da6869a53d59dae087cd
Vinícius Valle Beraldo <vvberaldo@proton.me>
2025-05-15 08:35:30 -0300

    criado metodo que seta o tipo do pattern e converte para um pattern correto que depois e criado um parâmetro a mais para pegar o middleware que a rota passa, editando também o o metodo que verifica se o pattern é válido

--- App/src/Core/Router.php
+++ App/src/Core/Router.php
@@ -2,6 +2,7 @@
 
 namespace App\Core;
 
+use App\Enums\RouteWildcard;
 use App\Exceptions\NotFoundException;
 
 class Router
@@ -11,13 +12,32 @@ class Router
         public readonly Request $request,
         public readonly Response $response,
     ){}
-    public function get(string $path, mixed $callback): void
+
+    public function replaceWildcardWithPattern(string $uriToReplace): string
     {
-        $this->routes['get'][$path] = $callback;
+        $patterns = [
+            '(:numeric)' => RouteWildcard::numeric->value,
+            '(:alpha)' => RouteWildcard::alpha->value,
+            '(:any)' => RouteWildcard::any->value,
+        ];
+        return str_replace(array_keys($patterns), array_values($patterns), $uriToReplace);
     }
-    public function post(string $path, mixed $callback): void
+    public function get(string $path, mixed $callback, ?array $middleware = null): void
     {
-        $this->routes['post'][$path] = $callback;
+        $newPath = $this->replaceWildcardWithPattern($path);
+        $this->routes['get'][$newPath] = [
+            'callback' => $callback,
+            'middleware' => $middleware,
+        ];
+    }
+    public function post(string $path, mixed $callback, ?array $middleware = null): void
+    {
+
+        $newPath = $this->replaceWildcardWithPattern($path);
+        $this->routes['post'][$newPath] = [
+            'callback' => $callback,
+            'middleware' => $middleware,
+        ];
     }
     /**
      * @throws NotFoundException
@@ -26,15 +46,28 @@ class Router
     {
         $path = $this->request->getPath();
         $method = $this->request->getMethod();
-        $callback = $this->routes[$method][$path] ?? false;
-        if(!$callback) {
-            throw new NotFoundException();
+        foreach ($this->routes[$method] as $pattern => $route) {
+            $regex = "#^" . $pattern . "$#";
+            if (preg_match($regex, $path, $matches)) {
+                array_shift($matches);
+                $callback = $route['callback'];
+                $middlewares = $route['middleware'] ?? [];
+                foreach ($middlewares as $middleware) {
+                    $middlewareClass = "\\App\\Middleware\\" . ucfirst($middleware) . "Middleware";
+                    if (!class_exists($middlewareClass)) {
+                        throw new \Exception("Middleware '$middlewareClass' not found.");
+                    }
+                    $instance = new $middlewareClass();
+                    $instance->handle($this->request);
+                }
+                if (is_array($callback)) {
+                    $controller = new $callback[0]();
+                    $controller->action = $callback[1];
+                    $callback[0] = $controller;
+                }
+                return call_user_func_array($callback, array_merge([$this->request], $matches));
+            }
         }
-        if(is_array($callback)) {
-            $controller = new $callback[0]();
-            $controller->action = $callback[1];
-            $callback[0] = $controller;
-        }
-        return call_user_func($callback, $this->request, $this->response);
+        throw new NotFoundException();
     }
 }
