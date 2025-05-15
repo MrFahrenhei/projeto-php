2df9c9471b8607dd4b77c4f516f1ee0fd46e5c22
Vinícius Valle Beraldo <vvberaldo@proton.me>
2025-05-15 08:41:24 -0300

    enviando a classe que define e cria tokens jwt

---
+++ App/src/Core/JwtAuth.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Core;
+
+use DateTimeImmutable;
+use Exception;
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class JwtAuth
+{
+    public static function generate(array $payload): string
+    {
+        return JWT::encode($payload, getenv('SECRET_KEY'), 'HS256');
+    }
+    public static function decode(string $token): ?array
+    {
+        try {
+            return (array) JWT::decode($token, new Key(getenv('SECRET_KEY'), 'HS256'));
+        } catch (Exception $e) {
+            return null;
+        }
+    }
+    public static function generateToken(array $payload): string
+    {
+        $issuedAt = new DateTimeImmutable();
+        $exp = $issuedAt->modify('+6 minutes')->getTimestamp();
+        $newPayload = [
+            'iss' => $payload['iss'] ?? 'Avetools',
+            'type' => $payload['type'],
+            'sub' => $payload['sub'],
+            'name' => $payload['name'],
+            'iat' => $issuedAt->getTimestamp(),
+            'exp' => $exp,
+            'nbf' => $issuedAt->getTimestamp(),
+        ];
+        return JWT::encode($newPayload, getenv('SECRET_KEY'), 'HS256');
+    }
+}
